383ac778b075fbf8fa9aebcc34ccf3bd753eded5
Júlio Pereira Machado <julioapm@gmail.com>
2019-07-25 14:04:17 -0300

    Teste unitário com mock-http

--- src/controllers/croptypeController.test.ts
+++ src/controllers/croptypeController.test.ts
@@ -1,17 +1,41 @@
 import * as repository from '../persistence/croptypeRepository';
 import * as controller from './croptypeController';
+import * as httpmocks from 'node-mocks-http';
+import { CropType } from '../models/croptype';
 
 describe('Teste unitário CropType controller', () => {
-    test('getCropTypes() deve chamar croptypeRepository.findAll()', async () => {
-        fail();
-        /*
-        const req = {body:{}};
-        const findAllMock = jest.spyOn(repository, 'findAll');
-        await controller.getCropTypes(req,res,next);
-        expect(findAllMock).toHaveBeenCalled();
-        */
-    });
-    test('getCropTypes() deve retornar', () => {
-        fail();
+    describe('getCropTypes()', () => {
+        test('deve retornar status 200 OK, JSON, corpo array vazio', async () => {
+            const req = httpmocks.createRequest({
+                method: 'GET',
+                url: '/croptypes'
+            });
+            const res = httpmocks.createResponse();
+            const findAllMock = jest.spyOn(repository, 'findAll');
+            findAllMock.mockImplementationOnce(async () => []);
+            await controller.getCropTypes(req,res,{} as any);
+            expect(findAllMock).toHaveBeenCalled();
+            expect(res._isJSON()).toBe(true);
+            expect(res.statusCode).toBe(200);
+            expect(res._getJSONData().length).toBe(0);
+        });
+        test('deve retornar status 200 OK, JSON, corpo objeto croptype', async () => {
+            const req = httpmocks.createRequest({
+                method: 'GET',
+                url: '/croptypes'
+            });
+            const res = httpmocks.createResponse();
+            let croptype: CropType = {uid:'1', name:'teste', lower:50, upper:60};
+            const findAllMock = jest.spyOn(repository, 'findAll');
+            findAllMock.mockImplementationOnce(async () => [croptype]);
+            await controller.getCropTypes(req,res,{} as any);
+            expect(findAllMock).toHaveBeenCalled();
+            expect(res._isJSON()).toBe(true);
+            expect(res.statusCode).toBe(200);
+            expect(res._getJSONData().length).toBe(1);
+            expect(res._getJSONData()[0].name).toBe('teste');
+            expect(res._getJSONData()[0].lower).toBe(50);
+            expect(res._getJSONData()[0].upper).toBe(60);
+        });
     });
 });
